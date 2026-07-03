1b7bb980f0bae72e06fa47255e9975e8cd4d38b7
Ido <dev.xyzuniverse@gmail.com>
2026-07-03 14:44:36 +0200

    rodin: Build missing ims permissions
    Change-Id: Id6deb88f051d2d0dea2cc451882529d392788684
    Signed-off-by: Ido <dev.xyzuniverse@gmail.com>

--- device.mk
+++ device.mk
@@ -363,6 +363,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml \
     frameworks/native/data/etc/android.software.ipsec_tunnel_migration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnel_migration.xml
 

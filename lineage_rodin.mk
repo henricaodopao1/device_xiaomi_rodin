f67e84d66ba51e8b535f7ca578662ede85ec7e46
Ido <dev.xyzuniverse@gmail.com>
2026-07-08 21:56:18 +0200

    rodin: Fixup build fingerprint
    Change-Id: I06a6a6fa2ccf053b4c50a86302c6d1a395161bb2
    Signed-off-by: Ido <dev.xyzuniverse@gmail.com>

--- lineage_rodin.mk
+++ lineage_rodin.mk
@@ -31,7 +31,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="missi-user 16 BP2A.250605.031.A3 16OS3.1.260413.104217578.MTPEGL.S release-keys" \
-    BuildFingerprint=POCO/rodin_eea/rodin:15/AP3A.240905.015.A2/OS3.0.300.0.WOJEUXM:user/release-keys \
+    BuildFingerprint=POCO/rodin_eea/rodin:16/BP2A.250605.031.A3/OS3.0.300.0.WOJEUXM:user/release-keys \
     DeviceName=$(PRODUCT_SYSTEM_DEVICE) \
     DeviceProduct=$(PRODUCT_SYSTEM_NAME) \
     SystemDevice=$(PRODUCT_SYSTEM_DEVICE) \
